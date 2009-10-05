911808113050c2d9a8102208917172de3d1edf0b
winhamwr <winhamwr@gmail.com>
2009-10-04 21:51:21 -0400

    Changed the attendance model layout a bit and added django admin integration.

--- django_attendance/models.py
+++ django_attendance/models.py
@@ -1,23 +1,47 @@
+import datetime
+
 from django.db import models
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.contrib.auth.models import User
+from django.contrib import admin
 
 from schedule.models import Occurrence
 
-class Attendance(models.Model):
+class EventAttendance(models.Model):
     '''
     The attendance status of a user for a specific Occurrence of an event.
     '''
-    user = models.ForeignKey(User)
-    occurrence = models.ForeignKey(Occurrence)
-    attended = models.BooleanField(default=False)
-
-    attendance_recorder = models.ForeignKey(User, related_name='recorded_attendances')
-    attendance_recorded_date = models.DateTimeField(default=datetime.datetime.now)
+    occurrence = models.OneToOneField(Occurrence)
+    attendee_statuses = models.ManyToManyField(User, through='AttendanceStatus')
 
     class Meta:
         verbose_name = _('attendance')
         verbose_name_plural = _('attendances')
 
     def __unicode__(self):
-        return "Attendance for %s at %s" % (self.user, self.occurrence)
+        return "Attendance for %s-%s" % (self.occurrence.title,
+                                         self.occurrence.start)
+
+
+class AttendanceStatus(models.Model):
+    event_attendance = models.ForeignKey(EventAttendance)
+    user = models.ForeignKey(User)
+    attended = models.BooleanField(default=False)
+
+    attendance_recorded_date = models.DateTimeField(default=datetime.datetime.now)
+
+    def __unicode__(self):
+        return "Attendance status for %s at %s-%s" % (self.user,
+                                                      self.event_attendance.occurrence.title,
+                                                      self.event_attendance.occurrence.start)
+
+class AttendanceStatusInline(admin.StackedInline):
+    model = AttendanceStatus
+    exclude = ('attendance_recorded_date',)
+    extra = 5
+
+class EventAttendanceAdmin(admin.ModelAdmin):
+    inlines = [AttendanceStatusInline]
+
+
+admin.site.register(EventAttendance, EventAttendanceAdmin)
